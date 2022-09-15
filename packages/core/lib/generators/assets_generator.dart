9a3b29c7b2b1d71e07765968fdd07cc1df312105
anKii <41232001+ankiimation@users.noreply.github.com>
2022-09-15 14:22:03 +0700

    update

--- packages/core/lib/generators/assets_generator.dart
+++ packages/core/lib/generators/assets_generator.dart
@@ -483,20 +483,68 @@ class _Statement {
       !keywords.contains(name);
 }
 
-const keywords = '''
-abstract 	else	import 	show 
-as 	enum	in	static 
-assert	export 	interface 	super
-async 	extends	is	switch
-await 	extension 	late 	sync 
-break	external 	library 	this
-case	factory 	mixin 	throw
-catch	false	new	true
-class	final	null	try
-const	finally	on 	typedef 
-continue	for	operator 	var
-covariant 	Function 	part 	void
-default	get 	required 	while
-deferred 	hide 	rethrow	with
-do	if	return	yield 
-dynamic 	implements 	set ''';
+const keywords = [
+  'abstract',
+  'else',
+  'import',
+  'show',
+  'as',
+  'enum',
+  'in',
+  'static',
+  'assert',
+  'export',
+  'interface',
+  'super',
+  'async',
+  'extends',
+  'is',
+  'switch',
+  'await',
+  'extension',
+  'late',
+  'sync',
+  'break',
+  'external',
+  'library',
+  'this',
+  'case',
+  'factory',
+  'mixin',
+  'throw',
+  'catch',
+  'false',
+  'new',
+  'true',
+  'class',
+  'final',
+  'null',
+  'try',
+  'const',
+  'finally',
+  'on',
+  'typedef',
+  'continue',
+  'for',
+  'operator',
+  'var',
+  'covariant',
+  'Function',
+  'part',
+  'void',
+  'default',
+  'get',
+  'required',
+  'while',
+  'deferred',
+  'hide',
+  'rethrow',
+  'with',
+  'do',
+  'if',
+  'return',
+  'yield',
+  'dynamic',
+  'implements',
+  'set'
+];
